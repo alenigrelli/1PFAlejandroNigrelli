1523284dae48bbb3c8c06a45cc394752b66b305c
alenigrelli <alejandro.nigrelli@gmail.com>
2022-04-25 23:45:13 -0300

    agrego toolbar al modulo core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ServiciosCursoService } from './servicios/servicios-curso.service';
 import { ServicioAlumnoService } from './servicios/servicio-alumno.service';
 import { SharedModule } from '../shared/shared.module';
+import { toolbarComponent } from './toolbar/toolbar.component';
 
 
 
 @NgModule({
   declarations: [
+    toolbarComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
-  providers: [ServicioAlumnoService, ServiciosCursoService]
+  exports:[toolbarComponent],
+  providers: [ServicioAlumnoService, ServiciosCursoService, toolbarComponent]
 })
 export class CoreModule { }
